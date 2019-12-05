c5fe3263822fac9813e617cc05cae4513d00f7fc
Adejobi Ezekiel <ezeko2017@gmail.com>
2019-12-05 17:49:06 +0100

    updated

--- backend/reg_validate.php
+++ backend/reg_validate.php
@@ -3,17 +3,18 @@ require_once('../config/connect.php');// call connection
 
 //save form values
 
-$surname = $_POST['surname'];
-$otherNames = $_POST['otherName'];
-$address = $_POST['address'];
-$phonenumber = $_POST['number'];
-$isMember = $_POST['member'];
-$group = $_POST['district'];
-$location = $_POST['location'];
-$gender = $_POST['gender'];
-$denomination =$_POST['denomination'];
-$category = $_POST['category'];
-$age = $_POST['age'];
+$surname = filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_ENCODED);
+$otherNames = filter_input(INPUT_POST, 'otherName', FILTER_SANITIZE_ENCODED);
+$address =  filter_input(INPUT_POST, 'address', FILTER_SANITIZE_SPECIAL_CHARS);
+$phonenumber = filter_input(INPUT_POST, 'number', FILTER_SANITIZE_ENCODED);
+$isMember = filter_input(INPUT_POST, 'member', FILTER_SANITIZE_ENCODED);
+$group = filter_input(INPUT_POST, 'district', FILTER_SANITIZE_ENCODED);
+$location = filter_input(INPUT_POST, 'location', FILTER_SANITIZE_ENCODED);
+$gender = filter_input(INPUT_POST, 'gender', FILTER_SANITIZE_ENCODED);
+$denomination = filter_input(INPUT_POST, 'denomination', FILTER_SANITIZE_ENCODED);
+$category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_ENCODED);
+$age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_ENCODED);
+
 
 //check if user has been registered before
 
@@ -28,6 +29,35 @@ if ($result > 0){
 // If the details already exists, with an error message will be displayed.
 echo  "<script>alert('Participant Already Registered!!!'); window.location.replace('../register.html');</script>";
 }else{
+
+    switch($category){
+        //if adult add into adult database as well
+        case("adult"): $sql = "INSERT INTO `adult` (surname, otherName, H_address, phonenumber, isMember, district, C_location, denomination, gender, age, section) 
+        VALUES 
+        ('$surname', '$otherNames', '$address', '$phonenumber', '$isMember', '$group', '$location', '$denomination', '$gender', '$age', '$category')";
+        $query = mysqli_query($conn, $sql) or die(mysqli_error($conn)); break;
+
+        case("youth"): $sql = "INSERT INTO `youths` (surname, otherName, H_address, phonenumber, isMember, district, C_location, denomination, gender, age, section) 
+        VALUES 
+        ('$surname', '$otherNames', '$address', '$phonenumber', '$isMember', '$group', '$location', '$denomination', '$gender', '$age', '$category')";
+        $query = mysqli_query($conn, $sql) or die(mysqli_error($conn)); break;
+
+        case("campus"): $sql = "INSERT INTO `campus` (surname, otherName, H_address, phonenumber, isMember, district, C_location, denomination, gender, age, section) 
+        VALUES 
+        ('$surname', '$otherNames', '$address', '$phonenumber', '$isMember', '$group', '$location', '$denomination', '$gender', '$age', '$category')";
+        $query = mysqli_query($conn, $sql) or die(mysqli_error($conn)); break;
+
+        default: $sql = "INSERT INTO `children` (surname, otherName, H_address, phonenumber, isMember, district, C_location, denomination, gender, age, section) 
+        VALUES 
+        ('$surname', '$otherNames', '$address', '$phonenumber', '$isMember', '$group', '$location', '$denomination', '$gender', '$age', '$category')";
+    
+        $query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+    
+    
+    } continue;
+
+
+
     $sql = "INSERT INTO `participants` (surname, otherName, H_address, phonenumber, isMember, district, C_location, denomination, gender, age, section) 
     VALUES 
     ('$surname', '$otherNames', '$address', '$phonenumber', '$isMember', '$group', '$location', '$denomination', '$gender', '$age', '$category')";
